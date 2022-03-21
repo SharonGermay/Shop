93594681c812527ac4158d82ab784011e7afcd91
Germay1990 <shg1693@gmail.com>
2022-03-21 10:30:58 +0200

    fixed

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,24 @@
 //Reducer
 const cartReducer = (state = [], action) => {
-    switch (action.type) {
-      case "ADD":
-        let exist = state.find((item) => item.id === Number(action.data.id));
-  
-        if (exist) {
-          return [...state, { ...action.data, qty: action.data.qty + 1 }];
-        } else {
-          return [...state, { ...action.data, qty: 1 }];
-        }
-      case "REMOVE":
-        return state.filter(item => item.id !== action.data.id);
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case "ADD":
+      let exist = state.find((item) => item.id === Number(action.data.id));
 
-  export default cartReducer 
+      if (exist) {
+        state = state.map((item) =>
+          item.id === Number(action.data.id)
+            ? { ...item, qty: item.qty + 1 }
+            : item
+        );
+        return state;
+      } else {
+        return [...state, { ...action.data, qty: 1 }];
+      }
+    case "REMOVE":
+      return state.filter((item) => item.id !== action.data.id);
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
